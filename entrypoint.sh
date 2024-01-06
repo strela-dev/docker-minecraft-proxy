de1c6b74deede5a1c053ac1a80355619ec93204c
Philipp <p.eistrach@gmail.com>
2024-01-06 20:29:48 +0100

    refactor: remove useless comments [skip ci]

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Your custom actions here
-# For example:
 mkdir -p /server/plugins
 cp -r /tmp/plugins/* /server/plugins/
-
-# Now execute the original entrypoint "/start"
 
 /usr/bin/run-bungeecord.sh
